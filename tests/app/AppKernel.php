3e18d246d23e1c2ee3a59e5c8936129c1aac9a81
Chris Verges <cverges@coursehero.com>
2014-10-31 14:39:55 -0700

    Added AppKernel to support functional tests

---
+++ tests/app/AppKernel.php
@@ -0,0 +1,16 @@
+<?php
+
+use Symfony\Component\HttpKernel\Kernel;
+use Symfony\Component\Config\Loader\LoaderInterface;
+
+class AppKernel extends Kernel
+{
+    public function registerBundles()
+    {
+        return array();
+    }
+
+    public function registerContainerConfiguration(LoaderInterface $loader)
+    {
+    }
+}
